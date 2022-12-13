22f4d36ff08705e6c4aec942f55c2f00b04710d7
Matheus Nascimento <nasc.matheusfrancisco@gmail.com>
2022-12-13 19:48:52 -0300

    faltando texto e fotos

--- frontend/src/pages/Timeline.jsx
+++ frontend/src/pages/Timeline.jsx
@@ -6,21 +6,32 @@ class Timeline extends Component {
   render() {
     return (
       <>
-        <header>
+        <header className={styles.header}>
           <h1 className={styles.title}>Our Timeline</h1>
+          <div className={styles.titleDetail} />
           <h2 className={styles.subtitle}>remembering moments</h2>
         </header>
         <main>
-          {
-            moments.map((moment) => (
-              <div>
-                <h3>{moment.title}</h3>
-                <h4>{moment.date}</h4>
-                <p>{moment.text}</p>
-                <img src={moment.photo} alt="" srcset="" />
-              </div>
-            ))
-          }
+          <p>I know it's simple, but I made to remember our moments. You changed my life in a moment if I was thinking I never meet again someone if I can trust and have the oportunity to dream something together</p>
+          <section className={styles.timeline}>
+            {
+              moments.map((moment) => (
+                <div>
+                  <div className={styles.momentHeader}>
+                    <h3>{moment.title}</h3>
+                    <h4>{moment.date}</h4>
+                  </div>
+                  <div className={styles.photoContainer}>
+                    <img
+                      className={styles.photo}
+                      src={moment.photo}
+                      alt=""
+                    />
+                  </div>
+                </div>
+              ))
+            }
+          </section>
         </main>
       </>
     )
--- frontend/src/utils/moments.js
+++ frontend/src/utils/moments.js
@@ -1,9 +1,103 @@
 const moments = [
   {
-    title: 'First time I see you',
+    title: 'First time I saw you',
     date: 'April 8, 2022',
-    text: 'gfdghjfdhlkfglkhgfjkdfghj',
-    photo: 'https://drive.google.com/uc?export=view&id=1boGLOcAbQYXZjqSJexWYb0KhrUDN6WPN',
+    photo: 'https://drive.google.com/uc?export=view&id=1lkXdQFHQWdte-2Bw4td0PtP32SF4ALaT',
+  },
+  {
+    title: 'We meet again',
+    date: 'October 12, 2022',
+    photo: 'https://drive.google.com/uc?export=view&id=1lDruNuEIsEg4BG__p9RKs5m_OTWJI5fK',
+  },
+  {
+    title: 'The first kiss',
+    date: 'October 15, 2022',
+    photo: '',
+  },
+  {
+    title: 'First night as a couple',
+    date: 'October 22, 2022',
+    photo: '',
+  },
+  {
+    title: 'Halloween at Void',
+    date: 'October 28, 2022',
+    photo: '',
+  },
+  {
+    title: 'Cinema with your friends',
+    date: 'November 2, 2022',
+    photo: '',
+  },
+  {
+    title: 'You came to my house',
+    date: 'November 5, 2022',
+    photo: '',
+  },
+  {
+    title: 'First date, just us',
+    date: 'November 6, 2022',
+    photo: 'https://drive.google.com/uc?export=view&id=1QJFGNhj11PF8CIG_NfxrvbN7S0La0qgW',
+  },
+  {
+    title: 'Luiz Viado\'s day',
+    date: 'November 11, 2022',
+    photo: 'https://drive.google.com/uc?export=view&id=1S9mXsX4fdsUSm7byHsvr28M8zf0HriyN',
+  },
+  {
+    title: 'Drinks and cards',
+    date: 'November 19, 2022',
+    photo: '',
+  },
+  {
+    title: 'Meeting my family',
+    date: 'November 20, 2022',
+    photo: '',
+  },
+  {
+    title: 'I saw you singing',
+    date: 'November 24, 2022',
+    photo: '',
+  },
+  {
+    title: 'We started dating',
+    date: 'November 25, 2022',
+    photo: '',
+  },
+  {
+    title: 'My dad\'s birthday',
+    date: 'November 26, 2022',
+    photo: '',
+  },
+  {
+    title: 'My aunt\'s house',
+    date: 'November 27, 2022',
+    photo: '',
+  },
+  {
+    title: 'We watched Wednesday',
+    date: 'November 28, 2022',
+    photo: 'https://drive.google.com/uc?export=view&id=1Miwzt9flKgYqMSckkSHlGHSicTHU9-gj',
+  },
+  {
+    title: 'First call on discord',
+    date: 'December 4, 2022',
+    photo: 'https://drive.google.com/uc?export=view&id=1W86wyEzKTqiSr-t0tLUXFbfGGUW9VaNO',
+  },
+  {
+    title: 'Hours talking',
+    date: 'December 7, 2022',
+    photo: '',
+  },
+  {
+    title: 'Me, you and Steve',
+    date: 'December 10, 2022',
+    photo: 'https://drive.google.com/uc?export=view&id=1wnDf1gBlG170oF-JaSElkP_EoB2c1kh1',
+  },
+  {
+    title: 'You met my mother',
+    date: 'December 11, 2022',
+    photo: '',
   },
 ];
 
